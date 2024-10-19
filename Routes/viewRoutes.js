040acbf3e08fb82da899e45d884711947114f856
Your Name <saghafy.80@gmail.com>
2024-10-19 22:05:05 +0330

    put comments for viewRouter file

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -1,9 +1,13 @@
+// importing packages
 const express = require("express");
 
+// creating a router (subapplication)
 const router = express.Router();
 
+// defining routes
 router.get('/', (req, res) => {
     res.render('home');
 });
 
+// exporting the router module
 module.exports = router;
