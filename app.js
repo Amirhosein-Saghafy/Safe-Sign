a51dcb91994dcccd62d3ba0f19ff11ee7934c4e9
Your Name <saghafy.80@gmail.com>
2024-10-19 22:03:09 +0330

    Create viewRouter file for routes which blongs to rendering view pages and set view engine to ejs also create the first view page which is home page

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const helmet = require('helmet');
 
+// importing modules
+const viewRouter = require('./Routes/viewRoutes');
+
 // creating a new app
 const app = express();
 
+// Set ejs as template engine
+app.set('view engine', 'ejs');
+
 // using middlewares
+app.use(express.static('./Assets'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(helmet());
+
+// defining routes
+app.use('/', viewRouter);
 
 // exporting the app module
 module.exports = app;
