61ce036ea7fdc4c66b930bf914c49619a0489145
Your Name <saghafy.80@gmail.com>
2024-11-02 14:34:20 +0330

    Add some configuration for tailwind file also add font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./Views/**/*.ejs'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require("tailwindcss/defaultTheme");
 
+module.exports = {
+  content: ["./Views/**/*.ejs"],
+  theme: {
+    extend: {
+      fontFamily: {
+        IranSans: ["IranSans", ...defaultTheme.fontFamily.sans],
+        IranSansLight: ["IranSansLight", ...defaultTheme.fontFamily.sans],
+        IranSansBold: ["IranSansBold", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'xs': '400px',
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".rtl": {
+          direction: "rtl",
+        },
+        ".ltr": {
+          direction: "ltr",
+        },
+      });
+    },
+  ],
+};
